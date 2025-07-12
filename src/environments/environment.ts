aa1f402b5cf985ac04ba76c1f755fb7eda47cd2c
manigoldTLC <gabrxavier353@gmail.com>
2025-07-12 03:19:28 -0300

    correcao

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  whatsappNumber: '5562991525978',
+  contactEmail: '062embalagens@gmail.com',
+  formspreeEndpoint: 'https://formspree.io/f/xpwrveqp'
+};
